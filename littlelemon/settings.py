5d02ff310bfb4cd9f6bb9188265139d5c84729b8
Olha Chumbash <olyachumbash@gmail.com>
2025-10-04 22:20:47 +0200

    fix styles footer&header

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon_db',
+        'USER': 'littlelemon_user',
+        'PASSWORD': '1111',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
